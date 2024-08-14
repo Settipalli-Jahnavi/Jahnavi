d7ba36b7f6febcf1c7e6e0f2b62f0c4713cc526b
Settipalli-Jahnavi <jahnavi3692@gmail.com>
2024-08-14 13:01:07 +0530

    project done

---
+++ form.js
@@ -0,0 +1,9 @@
+function f1(){
+    var User=document.getElementById("User").value
+    var Pass=document.getElementById("Pass").value
+    
+    
+   document.write("USERNAME " + User\n)
+   
+   document.write("Password " + Pass\n)
+}
